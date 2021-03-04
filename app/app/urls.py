7207118792f545ec513b7fa0c77a1166c1c3bf70
shreyas <shreyashpm@gmail.com>
2021-03-04 19:54:49 +0530

    basic working project completed

--- app/app/urls.py
+++ app/app/urls.py
@@ -18,7 +18,7 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',views.home,name='home' ),
+    path('',views.home,name='home'),
     path('result/',views.results,name='result')
 
 ]
